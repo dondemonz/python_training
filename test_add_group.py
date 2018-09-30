ef124e372ae837518d6713aae23dde9b135d4971
Dmitry Efremov <kykareky05@mail.ru>
2018-09-30 20:01:03 +0300

    фикстура

--- test_add_group.py
+++ test_add_group.py
@@ -16,13 +16,8 @@ def test_add_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="asdsd", header="asdasd", footer="asddad"))
     app.logout()
-    """
-    def test_add_epmty_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
-        self.logout(wd)
-    """
+
+def test_add_epmty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
