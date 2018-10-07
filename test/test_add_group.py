826acc48d257265c3570f734ec859869cff44625
Dmitry Efremov <kykareky05@mail.ru>
2018-10-07 18:15:22 +0300

    интеллектуальность фикстуры

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,9 +3,8 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="asdsd", header="asdasd", footer="asddad"))
-    app.session.logout()
+
     """
 def test_add_epmty_group(app):
     app.session.login(username="admin", password="secret")
