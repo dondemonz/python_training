1b49fb1a650ba9f5230a6b88f41e15242200e24f
Dmitry Efremov <kykareky05@mail.ru>
2018-10-07 21:24:23 +0300

    предусловия для групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="asdsd", header="asdasd", footer="asddad"))
-    
+
 
 def test_add_epmty_group(app):
     app.group.create(Group(name="", header="", footer=""))
