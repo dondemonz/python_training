59487232b50687c95b583d5742c6fe58bc97f388
Dmitry Efremov <kykareky05@mail.ru>
2018-10-11 23:09:24 +0300

    загрузка информации в виде списков

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,8 +3,15 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="asdsd", header="asdasd", footer="asddad"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
+"""
 def test_add_epmty_group(app):
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+"""
--- test/test_group_modifiction.py
+++ test/test_group_modifiction.py
@@ -3,12 +3,19 @@ from model.group import Group
 def test_group_name_modification(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="zzzz_11"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 """
 def test_group_header_modification(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="zzz_11"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+
 """
