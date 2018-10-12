23832225e64acec380f7926f7424045d4cdada75
Dmitry Efremov <kykareky05@mail.ru>
2018-10-12 16:56:14 +0300

    сортировка списков групп перед сравнением

--- test/test_group_modifiction.py
+++ test/test_group_modifiction.py
@@ -4,10 +4,13 @@ def test_group_name_modification(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="zzzz_11"))
+    group = Group(name="zzzz_11")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups,  key=Group.id_or_max)
 
 """
 def test_group_header_modification(app):
