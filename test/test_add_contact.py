de12b6e30094c9e431cd3cf700c405ef46d95d1e
Dmitry Efremov <kykareky05@mail.ru>
2018-10-20 23:49:57 +0300

    параметризация создания контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-def test_add_contact(app):
+testdata = [Contact(firstname="", lastname="", middlename="")] + [
+    Contact(firstname=random_string("firstname", 5), lastname=random_string("lastname", 10), middlename=random_string("middlename", 15))
+    for i in range(5)
+]
+
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contacts_list()
-    contact = Contact(firstname="123qwe", lastname="123qwe")
     app.contact.create(contact)
     new_contacts = app.contact.get_contacts_list()
     assert len(old_contacts) + 1 == len(new_contacts)
@@ -18,20 +32,21 @@ contact = Contact(firstname="123qwe", middlename="123qwe", lastname="123qwe", ni
                        mobile="123qwe", work="123qwe", fax="123qwe", email="123qwe", email2="123qwe",
                        email3="123qwe", homepage="123qwe", bday="1", bmonth="January", byear="123q", aday="31",
                        amonth="December", ayear="9999", address2="123qwe", phone2="123qwe", notes="123qwe")
+"""
 
 """
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, contact):
+    old_groups = app.contact.get_contacts_list()
+    app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
+    new_contacts = app.contact.get_contacts_list()
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts,  key=Contact.id_or_max)
+
+
+
+contact = Contact(firstname="123qwe", lastname="123qwe")
 """
-def test_add_empty_contact(self):
-    wd = self.wd
-    self.open_home_page(wd)
-    self.login(wd, username="admin", password="secret")
-    self.create_contact(wd, Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
-                                        address="", home="",
-                                        mobile="", work="", fax="", email="", email2="", email3="", homepage="",
-                                        byear="", ayear="",
-                                        address2="", phone2="", notes="", image_path=""))
-    self.return_to_home_page(wd)
-    self.wd.implicitly_wait(30)
-    self.logout(wd)
-"""
+
 
