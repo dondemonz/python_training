2dceeff5e942c1668f5ea38193ff67d5b97020d6
Dmitry Efremov <kykareky05@mail.ru>
2018-11-04 16:11:27 +0300

    Переделаны тесты добавления, удаления и модификации группы, чтобы сравнивались данные загруженные из БД

--- test/test_group_modifiction.py
+++ test/test_group_modifiction.py
@@ -1,17 +1,18 @@
 from model.group import Group
-from random import randrange
+import random
 
-def test_group_name_modification(app):
-    if app.group.count() == 0:
+def test_group_name_modification(app, db):
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
+    old_groups = db.get_group_list()
+    random_group = random.choice(old_groups)
     group = Group(name="zzzz_11")
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(index, group)
-    new_groups = app.group.get_group_list()
+    group.id = random_group.id
+    app.group.modify_group_by_id(random_group.id, group)
+    new_groups = db.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[index] = group
+    old_groups.remove(random_group)
+    old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups,  key=Group.id_or_max)
 
 """
