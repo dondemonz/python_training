ea75e373c6089771d962123f12434dac24751a9a
Dmitry Efremov <kykareky05@mail.ru>
2018-11-05 10:13:22 +0300

    отключаемая проверка пользовательсного интерфейса для удаления группы

--- conftest.py
+++ conftest.py
@@ -37,9 +37,6 @@ def db(request):
     return dbfixture
 
 
-
-
-
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
@@ -48,10 +45,14 @@ def stop(request):
     request.addfinalizer(fin)
     return fixture
 
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption("--check_ui")
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="Chrome")
     parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--check_ui", action="store_true")
 
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
